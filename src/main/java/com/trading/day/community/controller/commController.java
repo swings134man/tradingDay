8b22ae36b04e8285797401d59f8c9605dee01899
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-10-11 22:42:46 +0900

    commu Controller add

---
+++ src/main/java/com/trading/day/community/controller/commController.java
@@ -0,0 +1,14 @@
+package com.trading.day.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+/************
+* @info : 커뮤니티 게시판 컨트롤러
+* @name : commController
+* @date : 2022/10/11 10:41 PM
+* @author : SeokJun Kang(swings134@gmail.com)
+* @version : 1.0.0
+************/
+@Controller
+public class commController {
+}
