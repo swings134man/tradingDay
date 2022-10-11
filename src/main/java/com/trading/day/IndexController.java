490e90e35cdc29c6766fd546e0f883db8c6dfb33
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-10-11 21:13:14 +0900

    index page add

---
+++ src/main/java/com/trading/day/IndexController.java
@@ -0,0 +1,15 @@
+package com.trading.day;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
