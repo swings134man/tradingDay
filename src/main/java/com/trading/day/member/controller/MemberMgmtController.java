96fc0fbe26657da898aa19697a04cf5999a60e66
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-10-11 21:15:09 +0900

    Member Controller add

---
+++ src/main/java/com/trading/day/member/controller/MemberMgmtController.java
@@ -0,0 +1,16 @@
+package com.trading.day.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/************
+* @info : 고객관리 컨트롤러
+* @name : MemberMgmtController
+* @date : 2022/10/11 9:14 PM
+* @author : SeokJun Kang(swings134@gmail.com)
+* @version : 1.0.0
+************/
+@Controller
+@RequiredArgsConstructor
+public class MemberMgmtController {
+}
