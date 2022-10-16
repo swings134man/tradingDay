2547040c43b39d62df4aaa3a80b977472ae4350e
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-10-16 16:22:00 +0900

    Swagger config fix - detail

--- src/main/java/com/trading/day/config/SwaggerConfig.java
+++ src/main/java/com/trading/day/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.trading.day.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,12 @@ public class SwaggerConfig {
     paths: apis 에 있는 API 중 특정 path 를 선택
     apiInfo:Swagger UI 로 노출할 정보
     */
+
+    private static final String SERVICE_NAME = "Trading Day";
+    private static final String API_VERSION = "V1";
+    private static final String API_DESCRIPTION = "Trading Day API TEST";
+    private static final String API_URL = "http://localhost:8080/";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -26,6 +34,16 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title(SERVICE_NAME) // 서비스명
+                .version(API_VERSION)                   // API 버전
+                .description(API_DESCRIPTION)           // API 설명
+                .termsOfServiceUrl(API_URL)             // 서비스 url
+                .licenseUrl("https://github.com/swings134man/tradingDay")
+                .build();
+
+    }// API INFO
 
 }
 
