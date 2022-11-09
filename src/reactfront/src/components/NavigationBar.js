aa65940d8d32f2739e455a9a26a3635b75d7e3b8
KIMTAEIL <caporatang@naver.com>
2022-11-10 03:53:59 +0900

    test

--- src/reactfront/src/components/NavigationBar.js
+++ src/reactfront/src/components/NavigationBar.js
@@ -5,10 +5,12 @@ function NavigationBar() {
         <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
 
             <div className="container px-5">
-                <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a className="navbar-brand" href="#page-top">Trading Day</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ms-auto">
+                        <li className="nav-item"><a className="nav-link" href="#!">커뮤니티</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#!">Trade!</a></li>
                         <li className="nav-item"><a className="nav-link" href="#!">Sign Up</a></li>
                         <li className="nav-item"><a className="nav-link" href="#!">Log In</a></li>
                     </ul>
