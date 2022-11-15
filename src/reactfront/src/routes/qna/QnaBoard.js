e5e22ab395f3b562f3283968f623556c391a2440
KIMTAEIL <caporatang@naver.com>
2022-11-15 17:16:13 +0900

    prop error

--- src/reactfront/src/routes/qna/QnaBoard.js
+++ src/reactfront/src/routes/qna/QnaBoard.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react';
 import TableTest from "../../components/TableTest";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Link} from "react-router-dom";
-import Paging from "../../components/Paging";
+
 import Pagination from "react-js-pagination";
 
 function QnaBoard() {
     const [qnaList, setQnaList] = useState([]);
+    const [totalElements, setTotalElements] = useState(0);
+
     const [page, setPage] = useState(1);
 
     const handlePageChange = (page) => {
@@ -18,9 +20,15 @@ function QnaBoard() {
             const res = await fetch(`/qna/v1/qnabylist?page=${page}`);
             const data = await res.json();
             setQnaList(data);
+            setTotalElements(data.totalElements);
         }
         getData();
+
     }, [page])
+
+
+
+
 
     return (
 
@@ -28,6 +36,7 @@ function QnaBoard() {
             <div align="center" style={{padding : 100, paddingRight: 330, paddingLeft: 330}}>
                 <div>
                     <h1>문의 게시판</h1>
+                    <h3>{qnaList.totalElements}</h3>
                 </div>
 
                 <TableTest data={qnaList} />
@@ -42,7 +51,7 @@ function QnaBoard() {
                 <Pagination
                     activePage={page} // 현재 페이지
                     itemsCountPerPage={10} // 한 페이지 당 보여줄 아이템 갯수
-                    totalItemsCount={qnaList.totalElements} // 총 아이템 갯수
+                    totalItemsCount={totalElements} // 총 아이템 갯수
                     pageRangeDisplayed={5} // paginator의 페이지 범위
                     prevPageText={"‹"} // "이전"을 나타낼 텍스트
                     nextPageText={"›"} // "다음"을 나타낼 텍스트
