188b8c5ecdc7bee5c875075c0e4ed5fc576e7314
KIMTAEIL <caporatang@naver.com>
2022-11-20 22:22:45 +0900

    체크박스 체크 조회조건 추가

--- src/reactfront/src/routes/qna/QnaBoard.js
+++ src/reactfront/src/routes/qna/QnaBoard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import TableTest from "../../components/TableTest";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Link} from "react-router-dom";
@@ -10,26 +10,58 @@ function QnaBoard() {
     const [totalElements, setTotalElements] = useState(0);
 
     const [page, setPage] = useState(1);
+    const [isCheckingBox, setIsCheckingBox] = useState(false)
+
+    // TODO : 로그인 구현 되면 세션 아이디로 파라메터 대체.
+    const id = "xodlf5363";
+
+    const checkingCheckedBox = (e) => {
+        if (e.target.checked) {
+            setIsCheckingBox(true);
+        } else if(!e.target.checked) {
+            setIsCheckingBox(false);
+        }
+    }
 
     const handlePageChange = (page) => {
         setPage(page);
     };
 
     useEffect(() => {
-        const getData = async () => {
-            const res = await fetch(`/qna/v1/qnabylist?page=${page}`);
-            const data = await res.json();
-            setQnaList(data);
-            setTotalElements(data.totalElements);
+        if(isCheckingBox) {
+            const getData = async () => {
+                const res = await fetch(`/qna/v1/qnaByWriter?page=${page}&writer=${id}`);
+                const data = await res.json();
+                console.log("나는 부분 조회 data", data)
+                setQnaList(data);
+                setTotalElements(data.totalElements);
+            }
+            getData();
+        } else {
+            const getData = async () => {
+                const res = await fetch(`/qna/v1/qnabylist?page=${page}`);
+                const data = await res.json();
+                console.log("나는 전체 조회 data", data)
+                setQnaList(data);
+                setTotalElements(data.totalElements);
+            }
+            getData();
         }
-        getData();
+    }, [isCheckingBox, page])
 
-    }, [page])
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         const res = await fetch(`/qna/v1/qnabylist?page=${page}`);
+    //         const data = await res.json();
+    //         setQnaList(data);
+    //         setTotalElements(data.totalElements);
+    //     }
+    //     getData();
+    // }, [page])
 
     return (
 
         <div>
-            {/*<div align="center" style={{padding : 100, paddingRight: 330, paddingLeft: 330}}>*/}
             <div align="center" style={{padding : 100}}>
                 <div>
                     <h1>문의 게시판</h1>
@@ -42,6 +74,11 @@ function QnaBoard() {
                         문의하기
                         </Link>
                     </button>
+                </div>
+                <div align="center" >
+                {/*<Label><input type="checkbox" />체크박스 선택시 내가 작성한 글만 볼 수 있습니다.</Label>*/}
+                    <input type="checkbox" style={{margin: 3, zoom: 1.5}} onClick={checkingCheckedBox}  />
+                    <label style={{margin: 3 }}>체크박스 선택시 내가 작성한 글만 볼 수 있습니다.</label>
                 </div>
                 <Pagination
                     activePage={page} // 현재 페이지
