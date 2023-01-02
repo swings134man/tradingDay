c8c7b3344e3b8798f125db517e30afaa516d1ea1
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-01-02 22:32:20 +0900

    spinner design

--- src/reactfront/src/components/Loader.js
+++ src/reactfront/src/components/Loader.js
@@ -4,20 +4,26 @@ import ReactLoading from 'react-loading'
 function Loader({type, color, message}) {
 
     return (
-        <div class="contentWrap" style={{alignContent: "center"}}>
+        <div className="d-flex  justify-content-center">
+         {/*<div class="contentWrap" style={{alignContent: "center"}}>*/}
             <div style={{
                 position: "fixed",
-                top: "45%",
-                left: "55%",
+                // top: "45%",
+                // left: "55%",
+                top: "50%",
+                left: "50%",
                 transform: "translate(-50%, -50%)"
             }}>
-                <h4>{message}</h4>
+
+               <div className="d-flex  justify-content-center"> <h4>{message}</h4></div>
                 {/*<h3>창을 닫지 말아주세요.</h3>*/}
+                <div className="d-flex  justify-content-center">
                 <ReactLoading
                     type={type}
                     color={color}
                     height={'40%'}
                     width={'40%'} />
+                </div>
             </div>
         </div>
     );
