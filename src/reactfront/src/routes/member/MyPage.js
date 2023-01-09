8f4cbab73c23960998d93c22f33f1ef97d8a1bb8
KIMTAEIL <caporatang@naver.com>
2023-01-09 15:30:08 +0900

    노트 분기

--- src/reactfront/src/routes/member/MyPage.js
+++ src/reactfront/src/routes/member/MyPage.js
@@ -3,39 +3,33 @@ import PersonInfoModi from "./PersonInfoModi";
 import PersonPwdChk from "./PersonPwdChk";
 import {render} from "react-dom";
 import ApplyBoardList from "../apply/ApplyBoardList";
+import Note from "../../components/Note";
+import NoteBoardList from "../note/NoteBoardList";
 // import {mypageCss} from
 
 
 
-function MyPage(props) {
+function MyPage() {
 
-    // false인 경우 마이 페이지 렌더링, true인 경우 지원서 페이지 렌더링 온클릭에 넣어서 분기
-    const [quarter, setQuarter] = useState();
-    //const [memberPwdChk, setMemberPwdChk] = useState(false);
+    const [quarter, setQuarter] = useState("apply");
 
-    const memberPwdChk = () => {
-        if(props.pwdChk) {
-            setQuarter(false);
-        }
-    }
-
-
-    useEffect(() => {
-
-    }, [props.pwdChk])
 
 
     const pageRender = (page) => {
         let pageId = page.target.getAttribute("data-id")
-        if(pageId === "mypage") {
-            setQuarter(false)
-        } else if( pageId === "apply") {
-            setQuarter(true)
+
+        if(pageId === "apply") {
+            setQuarter("apply");
+        }
+        if( pageId === "myPage") {
+            setQuarter("myPage");
+        }
+        if(pageId === "note") {
+            setQuarter("note");
         }
     }
 
     return (
-        // <div className="d-flex" >
         <div className="d-flex justify-content-center " style={{ paddingTop:100, height: '100vh'}}>
             <div className="border-end" id="sidebar-wrapper" >
                 <div className="sidebar-heading border-bottom bg-light" align="center">마이페이지</div>
@@ -43,26 +37,33 @@ function MyPage(props) {
                     <a className="list-group-item list-group-item-action list-group-item-light p-3"
                        data-id={"apply"}
                        onClick={pageRender}
-                       href="#!">지원서</a>
+                       href="#!">지원서
+                    </a>
                     <a className="list-group-item list-group-item-action list-group-item-light p-3"
                        href="#!"
-                       data-id={"mypage"}
-                       onClick={pageRender}>개인정보수정</a>
+                       data-id={"myPage"}
+                       onClick={pageRender}>개인정보수정
+                    </a>
+                    <a className="list-group-item list-group-item-action list-group-item-light p-3"
+                       href="#!"
+                       data-id={"note"}
+                       onClick={pageRender}>쪽지함
+                    </a>
                 </div>
             </div>
 
             <div >
                 <div style={{width: '100vh'}}>
-                    {quarter === false ? <PersonPwdChk /> : null }
+                    {quarter === "myPage" ? <PersonPwdChk /> : null }
                 </div>
                 <div style={{width: '100vh'}}>
-                    {quarter !== false ? <ApplyBoardList/> : null}
+                    {quarter === "apply"  ? <ApplyBoardList/> : null}
                 </div>
-
+                <div style={{width: '100vh'}}>
+                    {quarter === "note"  ? <NoteBoardList/> : null}
+                </div>
             </div>
         </div>
-
-        // </div>
    )
 }
 
