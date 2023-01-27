a4d988664bc4ef4724387f9c04241f2e4104b843
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-01-27 20:33:36 +0900

    Apllication 전역 설정 - For Batch

--- src/main/java/com/trading/day/DayApplication.java
+++ src/main/java/com/trading/day/DayApplication.java
@@ -1,15 +1,19 @@
 package com.trading.day;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableBatchProcessing // batch
+@EnableScheduling 	   // Scheduling
 public class DayApplication {
 
 	public static void main(String[] args) {
